1307a7752cde90d8d509325e4cc5f43a19f7471b
Hee0207 <hy.vohoangphuc@gmail.com>
2025-04-13 21:13:27 +0700

    chuong2.py

---
+++ [BOOK] Machine Learning Mastery With Python/Chuong_2/chuong2.py
@@ -0,0 +1,8 @@
+import scipy, numpy, matplotlib, pandas
+print('scipy:', scipy.__version__)
+print('numpy:', numpy.__version__)
+print('matplotlib:', matplotlib.__version__)
+print('pandas:', pandas.__version__)
+
+import sklearn
+print('sklearn:', sklearn.__version__)
