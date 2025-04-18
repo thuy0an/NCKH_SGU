3677e497c895e2ad62dda418a5a17fc03a05933f
Hee0207 <hy.vohoangphuc@gmail.com>
2025-04-18 22:35:18 +0700

    Create Peek at Your Data.py

---
+++ [BOOK] Machine Learning Mastery With Python/Chuong_2/5  Understand Your Data With  Descriptive Statistics /Peek at Your Data.py
@@ -0,0 +1,5 @@
+from pandas import read_csv 
+filename = "pima-indians-diabetes.data.csv" 
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class'] data = read_csv(filename, names=names) 
+peek = data.head(20) 
+print(peek) 
