29e32ae58b9f5db22002f2fc4a1cfc81ba600ab6
Hee0207 <hy.vohoangphuc@gmail.com>
2025-04-18 22:05:48 +0700

    Delete [BOOK] Machine Learning Mastery With Python/Chuong_2/bai2.py

--- [BOOK] Machine Learning Mastery With Python/Chuong_2/bai2.py
+++
@@ -1,8 +0,0 @@
-import scipy, numpy, matplotlib, pandas
-print('scipy:', scipy.__version__)
-print('numpy:', numpy.__version__)
-print('matplotlib:', matplotlib.__version__)
-print('pandas:', pandas.__version__)
-
-import sklearn
-print('sklearn:', sklearn.__version__)
