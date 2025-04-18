2fb0869373890623df17f0c279a24a6dc260805c
Hee0207 <hy.vohoangphuc@gmail.com>
2025-04-18 22:20:34 +0700

    Create Matplotlib Crash Course.py

---
+++ [BOOK] Machine Learning Mastery With Python/Chuong_2/3 Crash Course in Python and SciPy/Matplotlib Crash Course.py
@@ -0,0 +1,19 @@
+#3.3
+#3.3.1 Line Plot 
+import matplotlib.pyplot as plt
+import numpy
+
+myarray = numpy.array([1, 2, 3])
+plt.plot(myarray)
+plt.xlabel('some x axis')
+plt.ylabel('some y axis')
+plt.show()
+#3.3.2 Scatter Plot
+import matplotlib.pyplot as plt
+import numpy
+
+myarray = numpy.array([1, 2, 3])
+plt.plot(myarray)
+plt.xlabel('some x axis')
+plt.ylabel('some y axis')
+plt.show()
