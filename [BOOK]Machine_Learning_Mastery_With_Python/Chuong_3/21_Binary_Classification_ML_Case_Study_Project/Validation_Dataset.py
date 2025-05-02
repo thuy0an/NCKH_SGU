6b3f577c4281e67918d84d919f9d2d26127573f7
langvuong09 <121181317+langvuong09@users.noreply.github.com>
2025-05-02 10:18:38 +0700

    Update Validation_Dataset.py

--- [BOOK]Machine_Learning_Mastery_With_Python/Chuong_3/21_Binary_Classification_ML_Case_Study_Project/Validation_Dataset.py
+++ [BOOK]Machine_Learning_Mastery_With_Python/Chuong_3/21_Binary_Classification_ML_Case_Study_Project/Validation_Dataset.py
@@ -4,5 +4,5 @@ X = array[:,0:60].astype(float)
 Y = array[:,60]
 validation_size = 0.20
 seed = 7
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,
-test_size=validation_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,test_size=validation_size, random_state=seed)
+
